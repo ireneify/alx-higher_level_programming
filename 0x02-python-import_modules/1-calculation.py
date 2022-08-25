46fd39959c8cff86afcf784a9dd5000524dc05be
ireneify <onyealisiifeoma@gmail.com>
2022-08-25 14:56:36 -0700

    import

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+if __name__ == '__main__':
+    """
+    Prints the sum of the addition, substract, multiplication
+     and division between two numbers
+     """
+     a = 10
+
+     print("{} + {} = {}".format(a, b, add(a, b)))
+     print("{} - {} = {}".format(a, b, sub(a, b)))
+     print("{} * {} = {}".format(a, b, mul(a, b)))
+     print("{} / {} = {}".format(a, b, div(a, b)))
