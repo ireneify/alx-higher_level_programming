a65e52073d810d91dc9b0136327b115c515adde2
Onyealisi Ifeoma Irene <onyealisiifeoma@gmail.com>
2022-08-25 15:13:47 -0700

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == '__main__':
     """Prints the argument list passed to the program"""
-    
+
     total = 0
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
